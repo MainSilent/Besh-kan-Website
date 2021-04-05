fa22aefd4352100d81783773795d5bfba8b39b9a
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-04-05 12:58:04 +0430

    init

---
+++ src/js/scripts.js
@@ -0,0 +1,27 @@
+/* Template: Aria - Business HTML Landing Page Template
+   Author: Inovatik
+   Created: Jul 2019
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	document.onreadystatechange = function(e)
+    {
+        $("nav, header").hide()
+    }
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+                $("nav, header").show();
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+})(jQuery);
